b18422949b203758f3533bb017d7d114555ea8cf
Your Name <you@example.com>
2026-01-13 18:57:53 +0530

    Fix logo path for production

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -46,7 +46,7 @@ const Loading = ({ percent }: { percent: number }) => {
     <>
       <div className="loading-header">
         <a href="/#" className="loader-title" data-cursor="disable">
-          <img src="/logo.png" alt="Logo" className="loader-logo" />
+          <img src="/images/logo.png" alt="Logo" className="loader-logo" />
         </a>
         <div className={`loaderGame ${clicked && "loader-out"}`}>
           <div className="loaderGame-container">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          <img src="/logo.png" alt="Logo" className="navbar-logo" />
+          <img src="/images/logo.png" alt="Logo" className="navbar-logo" />
         </a>
         <a
           href="mailto:bannysukumar@gmail.com"
